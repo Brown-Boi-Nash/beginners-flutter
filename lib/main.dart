99dd51bfbb5cdab28cfd1d1fa0ecefb814106711
Brown-Boi-Nash <shetty.nishan98@gmail.com>
2021-07-24 22:07:53 +0530

    Second commit: Adding stateful widget to generate random words

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,20 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,7 +28,8 @@ class MyApp extends StatelessWidget {
           title: Text("Welcome to flutter"),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          //child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
